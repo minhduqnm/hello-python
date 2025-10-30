de5f1b811aab8e222df5d6a6d477455323b9c1f4
minhduqnm <minhduqnm@gmail.com>
2025-10-30 22:51:23 +0700

    Sửa test_main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def add(a, b):
+    return a + b
 if __name__ == "__main__":
-"print('Hello GitHub!')" 
-print("Xin chào từ GitHub!")
-print("Đây là bản chạy thử nghiệm trên branch dev")
-print("Add GitHub Actions test workflow")
+ print(add(2, 5))
